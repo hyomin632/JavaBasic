f764e1bbe5d08cfdf1c995d1c8c42fe3f9a7e6a0
hyomin632 <hyomin632@gmail.com>
2021-03-18 13:17:16 +0900

    연산자 형변

---
+++ src/Celeste/basic/Day03/EX07.java
@@ -0,0 +1,33 @@
+package Celeste.basic.Day03;
+
+public class EX07 {
+   public static void main(String[] args) {
+	   
+	   System.out.println(3 + 4.5 * 2 + 27 / 8);
+	   
+	   System.out.println(true || false && 3 < 4 || !(5 == 7));
+	   
+	   boolean result = (3 < 5) & (6 >= 2);
+	   System.out.println("(3 < 5) and (6 >= 2)  + result");
+	   
+	   // System.out.println(!true > 'A');
+	   
+	   System.out.println(7 % 4 + 3 - 2 / 6 * 'Z');
+	   
+	   System.out.println('D' + 1 + 'M' % 2 / 3);
+	   
+	   System.out.println(5.0 / 3 + 3 / 3);
+	   
+	   System.out.println(53 % 21 < 45 / 18);
+	   
+	   System.out.println((4 < 6) || true && false || false && (2 > 3));
+	   
+	   System.out.println(7 - (3 + 8 * 6 + 3) - (2 + 5 * 2));
+	   
+	   
+	   
+	   
+	   
+	   
+   }
+}
---
+++ src/Celeste/basic/Day03/EX08.java
@@ -0,0 +1,35 @@
+package Celeste.basic.Day03;
+
+public class EX08 {
+   public static void main(String[] args) {
+	   
+	   System.out.print( " May 13, 1988 fell on day number ");
+	   
+	   System.out.println( ( (13 + (13 * 3 - 1) / 5 + 1988 % 100 + 1988 % 100 / 4 + 1988 / 400 - 2 * (1988 / 100) ) % 7 + 7) % 7 );
+  
+	   System.out.print( " Check out this line  ");
+	   
+	   System.out.println( " //hello there " + '9' + 7 );
+	   
+	   System.out.print( 'H' + 'I' + " is " + 1 + " more example" );
+	   
+	   System.out.print( 'H' + 6.5 + 'I' + " is " + 1 + " more example" );
+	   
+	   System.out.printf( " Print both of us", " Me too" );
+	   
+	   System.out.print( " Reverse " + 'I' + 'T' );
+	   
+	   System.out.print( " No! Here is" + 1 + " more example" );
+	   
+	   System.out.println( " Here is " + 10 * 10); // that’s 100 ;
+	   
+	   System.out.println( " Not x is " + true ); // that’s true.
+	   
+	   // System.out.print(  );
+	   
+	   // System.out.println;
+	   
+	   System.out.print( " How about this one" + '?' + "Huh?" );
+	   
+   }
+}
