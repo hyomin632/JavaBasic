28abbc25df98c744ef9fd228f8d0d5e9c4ce46c9
celeste <celeste@localhost>
2021-03-22 17:25:41 +0900

    반복문 조건문 배

---
+++ src/Celeste/basic/Day05/CardCheck.java
@@ -0,0 +1,51 @@
+package Celeste.basic.Day05;
+
+import java.util.Scanner;
+
+public class CardCheck {
+    public static void main(String[] args) {
+        // 신용카드 번호를 보고 종류와 은행의 종류를 알려주는 프로그램
+        // 3: JCB카드
+        // 4: Visa카드
+        // 5: Master카드
+
+        String cardnum;
+        String cardtype = "";
+        String cardbank = "";
+        String fmt = "카드 종류는 %s, 은행은 %s입니다";
+        String result;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("카드번호를 입력하세요: ");
+        cardnum = sc.next();
+
+        // if (cardnum.charAt(0) == '3')
+        //     cardtype = "JCB 카드";
+        // else if (cardnum.charAt(0) == '4')
+        //     cardtype = "Visa 카드";
+        // else if (cardnum.charAt(0) == '5')
+        //     cardtype = "Master 카드";
+
+        switch(cardnum.charAt(0)) {
+            case '3': cardtype = "JCB 카드"; break;
+            case '4': cardtype = "Visa 카드"; break;
+            case '5': cardtype = "Master 카드"; break;
+        } // switch
+        // System.out.println(cardtype);
+
+        switch(cardnum) {
+            case "356317": cardbank = "NH농협카드"; break;
+            case "356901": cardbank = "신한카드"; break;
+            case "356912": cardbank = "KB국민카드"; break;
+            case "404825": cardbank = "비씨카드"; break;
+            case "438676": cardbank = "신한카드"; break;
+            case "457973": cardbank = "국민은행"; break;
+            case "515594": cardbank = "신한카드"; break;
+            case "524353": cardbank = "외환카드"; break;
+            case "540926": cardbank = "국민은행"; break;
+        }
+
+        System.out.printf(fmt, cardtype, cardbank);
+
+        }
+    }
---
+++ src/Celeste/basic/Day05/LeapYear.java
@@ -0,0 +1,37 @@
+package Celeste.basic.Day05;
+
+import java.util.Scanner;
+
+
+public class LeapYear {
+    public static void main(String[] args) {
+        // 윤년 여부 출력 프로그램
+        // 지구의 공전 주기와 자전 주기의 불일치로 발생
+        // 공전 주기: 365.24일
+        // 자전 주기: 0.24일
+        // 이 둘 사이의 오차로 인하여 4년마다 하루를 추가하는 방버으로 오류를 보정
+        // 조건 1: 4로 나누어 떨이지지만, 100으로 나누어 떨어지지 않음
+        // 조건 2: 400으로 나누어 떨어짐
+        // ex) 1600년(윤년), 2020(윤년), 1900(평년)
+
+        int year;
+        String result;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("연도를 입력하세요: ");
+        year = sc.nextInt();
+
+        if (year % 4 == 0 && year % 100 != 0 || (year % 400 == 0))
+            result = "윤년입니다";
+        else
+            result = "평년입니다";
+
+        System.out.println(result);
+
+
+
+    }
+}
+
+
+
+
