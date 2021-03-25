504fc4a24b8312d68fde7b1f600180d66bb10a7d
hyomin632 <hyomin632@gmail.com>
2021-03-25 18:10:05 +0900

    VO클래스 상속

---
+++ src/Celeste/basic/Day08/Person.java
@@ -0,0 +1,166 @@
+package Celeste.basic.Day08;
+ public class Person {
+    public static void main(String[] args) {
+        JavaBook j = new JavaBook();
+        j.setName("누구나 알기 쉬운 자바: 입문편");
+        j.setAuthor("카와바 타게시");
+        j.setTranslator("하진일");
+        j.setPublisher("혜지원");
+        j.setDate("2014년 06월");
+        j.setPrimeCost(25000);
+        j.setDisCost(22500);
+        j.setDiscount(10);
+        j.setSaving(10);
+
+        System.out.println(j.getName());
+        System.out.println(j.getAuthor());
+        System.out.println(j.getTranslator());
+        System.out.println(j.getPublisher());
+        System.out.println(j.getDate());
+        System.out.println(j.getPrimeCost());
+        System.out.println(j.getDisCost());
+        System.out.println(j.getDiscount());
+        System.out.println(j.getSaving());
+
+    }
+}
+class person {
+     private String name;
+     private String gender;
+     private int age;
+     private int height;
+     private int weight;
+
+    public person(String name, String gender, int age, int height, int weight) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
+
+class JavaBook {
+     private String name;
+     private String author;
+     private String translator;
+     private String publisher;
+     private String date;
+     private int primeCost;
+     private int disCost;
+     private double discount;
+     private int saving;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTranslator() {
+        return translator;
+    }
+
+    public void setTranslator(String translator) {
+        this.translator = translator;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getPrimeCost() {
+        return primeCost;
+    }
+
+    public void setPrimeCost(int primeCost) {
+        this.primeCost = primeCost;
+    }
+
+    public int getDisCost() {
+        return disCost;
+    }
+
+    public void setDisCost(int disCost) {
+        this.disCost = disCost;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getSaving() {
+        return saving;
+    }
+
+    public void setSaving(int saving) {
+        this.saving = saving;
+    }
+}
+
---
+++ src/Celeste/basic/Day08/ValueObject.java
@@ -0,0 +1,108 @@
+package Celeste.basic.Day08;
+
+public class ValueObject {
+    public static void main(String[] args) {
+        // setter / getter
+        // 기본적으로 클래스의 멤버변수의 접근지정자는 private으로 선언하는 것이 좋음 -> datahiding 차원
+        // 이렇게 선언하는 경우 외부 클래스에서 멤버변수에 접근이 불가능하게 되는데,
+        // 이때 setter/getter 메서드를 이용하면 접근불가변수를 조작할 수 있음
+
+        C g = new C();
+        // g.name = "혜교";
+        // g.kor = 99;
+        // g.eng = 75;
+        // g.mat = 64;
+        // 멤버변수가 private으로 선언되었으므로, 접근 불가 -> 값 대입 실패
+        // OOP에서는 객체의 변수를 외부에서 직접 접근하는 것을 권장하고 있지 않음
+        // 즉, 데이터는 외부에서의 접근을 막는다는 의미
+        // 만약 접근하려면 객체에서 공개한 메서드를 통해서만 가능하도록 유도 -> settler / getter
+
+        // setter를 이용해서 객체의 변수에 값 대입
+        g.setName("혜교");
+        g.setKor(87);
+        g.setEng(79);
+        g.setMat(97);
+
+        System.out.println(g.getName());
+        System.out.println(g.getKor());
+        System.out.println(g.getEng());
+        System.out.println(g.getMat());
+
+    }
+}
+
+class C {
+    private String name;
+    private int kor;
+    private int eng;
+    private int mat;
+
+    // setter 정의: public void set변수명(변수명) // set변수명에서 변수의 첫 글자는 대문자!
+    // 외부에서 객체의 멤버변수에 값을 설정할 때 사용
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+    public void setEng(int eng) {
+        this.eng = eng;
+    }
+    public void setMat(int mat) {
+        this.mat = mat;
+    }
+
+    // getter 정의: public 반환유형 get변수명() // get변수명에서 변수의 첫 글자는 대문자!
+    public String getName() {
+        return name;
+    }
+    public int getKor() {
+        return kor;
+    }
+    public int getEng() {
+        return eng;
+    }
+    public int getMat() {
+        return mat;
+    }
+}
+
+class D {
+    private String userid;
+    private String passwd;
+    private String email;
+
+    public D() { }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public D(String userid, String passwd, String email) {
+        this.userid = userid;
+        this.passwd = passwd;
+        this.email = email;
+    }
+}
+
+// 클래스 생성 순서: 멤버변수 - 기본 생성자 - 매개변수 생성자 - setter/getter
